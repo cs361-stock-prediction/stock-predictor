ceae6d415939a0d049209535f1a2a238a61e4ef4
Lyell Read <lyellread@gmail.com>
2020-03-02 20:31:51 -0800

    Adding basic server in Flask for #1

---
+++ server/server.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+# for login: https://www.youtube.com/watch?v=8aTnmsDMldY
+
+webserver = Flask(__name__)
+
+@webserver.route("/")
+def main():
+	return render_template("index.html")
+
+if __name__ == "__main__":
+	webserver.run(debug=True, port=4096)
