e7fa6ac85418d4bb5e8761c35b557811212dc8a8
Lyell Read <lyellread@gmail.com>
2020-03-03 16:41:28 -0800

    Fixing typo and adding some command indicators

--- scripts/install_requirements.sh
+++ scripts/install_requirements.sh
@@ -1,19 +1,19 @@
 echo " + Checking for Python 3"
 if command -v python3 >/dev/null 2>&1; then
-	echo "   Found python3!"
+	echo " + Found python3!"
 else
-	echo " ! No python 3 installed. Please install and retry!"
+	echo " E No python 3 installed. Please install and retry!"
 	exit 1
 fi
 
-if python3 -c "import flaskp" >/dev/null 2>&1; then
-	echo "   Flask already installed."
+if python3 -c "import flask" >/dev/null 2>&1; then
+	echo " + Flask already installed."
 else
 	echo " + Installing Flask for Python 3"
 	if pip3 install flask --user; then
-		echo "   Successfully installed flask!"
+		echo " + Successfully installed flask!"
 	else
-		echo " ! Pip3 failed to install flask. Exiting."
+		echo " E Pip3 failed to install flask. Exiting."
 		exit 1
 	fi
 fi
