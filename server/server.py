c7c2a26aace76f1bf831f82ff863150973e500be
Lyell Read <lyellread@gmail.com>
2020-03-04 12:40:25 -0800

    linting server.py

--- server/server.py
+++ server/server.py
@@ -4,9 +4,10 @@ from flask import Flask, render_template
 
 webserver = Flask(__name__)
 
+
 @webserver.route("/")
 def main():
-	return render_template("index.html")
+    return render_template("index.html")
 
 if __name__ == "__main__":
-	webserver.run(debug=True, host="0.0.0.0", port=4096)
+    webserver.run(debug=True, host="0.0.0.0", port=4096)
