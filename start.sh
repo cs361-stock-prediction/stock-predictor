b12d521c2c0c5367ec4eee936ca996aabe7a4ea8
Robert Detjens <detjensrobert@gmail.com>
2020-03-12 13:38:53 -0700

    use public localhost

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if ./scripts/install_requirements.sh; then
 	export PATH=$PATH:$HOME/.local/bin
 	export FLASK_APP=predictor.py
-	flask run --port 4096
+	flask run --host 0.0.0.0 --port 4096
 else
 	echo "Error installing requirements. Quitting"
 	exit 1
