83db0171efb38907693e6c26a805aaf287a3f82f
Lyell Read <lyellread@gmail.com>
2020-03-13 01:15:27 -0700

    Interstitial working 'prediction'

--- server/models.py
+++ server/models.py
@@ -3,6 +3,8 @@ from server import db, login
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
+import requests
+
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
--- server/routes.py
+++ server/routes.py
@@ -12,6 +12,8 @@ from server.forms import LoginForm, CreateAcctForm
 from flask_login import current_user, login_user, logout_user, login_required
 from server.models import User
 
+import plotly.graph_objects as go
+
 import os, requests
 
 # serve main files
@@ -48,15 +50,54 @@ def prediction(stock):
     json = resp.json()
 
     data = []
-
+    date = []
     for day in json['Time Series (Daily)']:
-        data.append([a, json['Time Series (Daily)'][day]['4. close']])
-
+        data.append(float(json['Time Series (Daily)'][day]['4. close']))
+        date.append(day)
+    
     data.reverse()
+    date.reverse()
 
-    print(data)
-
+    print (data)
+    print (date)
+    
     # Generate trendline
+
+
+
+
+    #turn dates to x values:
+
+    import datetime
+
+    datemask = "%Y-%m-%d"
+
+    datetimes = [datetime.datetime.strptime(x, datemask) for x in date]
+
+    base = datetimes[0]
+
+
+    x = [0]
+    [x.append((d1 - base).days) for d1 in datetimes[1:]]
+
+    
+    slope = ((data[-10] - data[-1]) / (x[-10] - x[-1]))
+
+    x1 = [x[-1]]
+    d1 = [data[-1]]
+
+    for n in range(0, 10):
+        d1.append(d1 [-1] + (1 * slope))
+        x1.append(x1[-1] +1)
+
+
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=x, y=data, name='Raw Data Graph',
+                            line=dict(color='royalblue', width=3)))
+    fig.add_trace(go.Scatter(x=x1, y=d1, name='Porj Graph',
+                            line=dict(color='aqua', width=3)))
+
+    fig.show()
 
 
 
